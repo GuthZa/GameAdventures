c65b65bb67d82c6e9ff0230bf36fadcee3e53d1f
Wisura <joao9.claro99@gmail.com>
2023-04-05 00:45:20 +0100

    Added locations

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -10,6 +10,7 @@ namespace Engine.ViewModels
     public class GameSession
     {
         public Player CurrentPlayer { get; set; }
+        public Locations CurrentLocation { get; set; }
         public GameSession()
         {
             CurrentPlayer = new Player();
@@ -19,6 +20,12 @@ namespace Engine.ViewModels
             CurrentPlayer.HitPoints = 10;
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
+            CurrentLocation = new Locations();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.XCoordinate = 0;
+            CurrentLocation.YCoordinate = -1;
+            CurrentLocation.Description = "This is your home";
+            CurrentLocation.ImageName = "/Engine;component/Images/Locations/Home.png";
         }
     }
 }
