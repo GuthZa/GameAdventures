efae61f9f1bcf1fd05fdaeb89add4aff8c36abd5
Wisura <joao9.claro99@gmail.com>
2023-04-05 10:58:35 +0100

    Added a world, with multiple locations

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -1,4 +1,5 @@
-﻿using Engine.Models;
+﻿using Engine.Factories;
+using Engine.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,8 @@ namespace Engine.ViewModels
     public class GameSession
     {
         public Player CurrentPlayer { get; set; }
-        public Locations CurrentLocation { get; set; }
+        public Location CurrentLocation { get; set; }
+        public World CurrentWorld { get; set; }
         public GameSession()
         {
             CurrentPlayer = new Player();
@@ -20,12 +22,9 @@ namespace Engine.ViewModels
             CurrentPlayer.HitPoints = 10;
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
-            CurrentLocation = new Locations();
-            CurrentLocation.Name = "Home";
-            CurrentLocation.XCoordinate = 0;
-            CurrentLocation.YCoordinate = -1;
-            CurrentLocation.Description = "This is your home";
-            CurrentLocation.ImageName = "/Engine;component/Images/Locations/Home.png";
+            WorldFactory factory = new WorldFactory();
+            CurrentWorld = factory.CreateWorld();
+            CurrentLocation = CurrentWorld.LocationAt(0, 0);
         }
     }
 }
