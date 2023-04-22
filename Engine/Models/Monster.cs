d4c8e9d28ebd8cb394767b347859afb18bc13353
Wisura <joao9.claro99@gmail.com>
2023-04-22 16:50:51 +0100

    Added load Monster data from xml files

--- Engine/Models/Monster.cs
+++ Engine/Models/Monster.cs
@@ -1,23 +1,46 @@
-﻿using System;
+﻿using Engine.Factories;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Engine.Models
 {
     public class Monster : LivingEntity
     {
+        private readonly List<ItemPercentage> _lootTable = new List<ItemPercentage>();
+        public int ID { get; }
         public string ImageName { get; }
         public int RewardExperiencePoints { get; }
-        public Monster(string name, string imageName,
-            int maximumHitPoints, int currenthitPoints,
+        public Monster(int id, string name, string imageName,
+            int maximumHitPoints,
+            GameItem currentWeapon,
             int rewardExperiencePoints, int gold)
-            : base(name, maximumHitPoints, currenthitPoints, gold)
+            : base(name, maximumHitPoints, maximumHitPoints, gold)
         {
-            ImageName = $"/Engine;component/Images/Monsters/{imageName}";
+            ID = id;
+            ImageName = imageName;
+            CurrentWeapon = currentWeapon;
             RewardExperiencePoints = rewardExperiencePoints;
+        }
+        public void AddItemToLootTable(int id, int percentage)
+        {
+            //Removes the entry if already exists
+            _lootTable.RemoveAll(ip => ip.ID == id);
+            _lootTable.Add(new ItemPercentage(id, percentage));
+        }
+        public Monster GetNewInstance()
+        {
+            //Clones this monster to a new Monster object
+            Monster newMonster = new Monster(ID, Name, ImageName, MaximumHitPoints, CurrentWeapon, RewardExperiencePoints, Gold);
+            foreach(ItemPercentage itemPercentage in _lootTable)
+            {
+                //Clone the loot table
+                newMonster.AddItemToLootTable(itemPercentage.ID, itemPercentage.Percentage);
+                //Populate the inventory
+                if(RandomNumberGenerator.NumberBetween(1,100) <= itemPercentage.Percentage)
+                {
+                    newMonster.AddItemToInventory(ItemFactory.CreateGameItem(itemPercentage.ID));
+                }
+            }
+            return newMonster;
         }
     }
 }
